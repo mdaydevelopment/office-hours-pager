fbe90b0f3aba4a94bb1167d300eecf8da2b14eb2
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2019-11-15 09:54:28 -0600

    Updated Doorbell.sql

--- sql/doorbell.sql
+++ sql/doorbell.sql
@@ -7,18 +7,18 @@ use doorbell;
 --drop user 'kelli'@'localhost';
 --drop user 'michelle'@'localhost' ;
 
-create user 'kelli'@'localhost' identified by 'password';
-create user 'michelle'@'localhost' identified by 'password';
+--create user 'kelli'@'localhost' identified by 'password';
+--create user 'michelle'@'localhost' identified by 'password';
 
-grant all on doorbell.* to 'kelli'@'localhost';
-grant all on doorbell.* to 'michelle'@'localhost';
+--grant all on doorbell.* to 'kelli'@'localhost';
+--grant all on doorbell.* to 'michelle'@'localhost';
 
-create table users (userID int not null primary key auto_increment, name varchar(20) not null, isAdmin Boolean, userpassword varchar(15));
+create table users (userID int not null primary key auto_increment, name varchar(255) not null, isAdmin Boolean, userpassword varchar(255));
 
 create table terms (termID int not null primary key auto_increment, startdate date, enddate date);
 
 create table office_hours (id int not null primary key auto_increment, userID int not null, 
-termID int not null, day_of_week date, hourstart date, hourend date, foreign key (userID) references users(userID),
+termID int not null, day_of_week date, hourstart time, hourend time, foreign key (userID) references users(userID),
 foreign key (termID) references terms(termID));
 
 create table requests (id int not null primary key auto_increment, userID int not null, foreign key (userID) references users(userID));
