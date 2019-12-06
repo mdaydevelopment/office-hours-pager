4aef7ab3f13c55c13622b0e2b3eec3c82a4b03b5
Adam Baszczynski <acbaszczynski@dmacc.edu>
2019-12-06 10:42:24 -0600

    Webpage commit
    - Updated main landing login page
    - added instructor office hour creation page
    - added logout.php and session.php stubs

--- www/login.php
+++ www/login.php
@@ -1,36 +1,41 @@
 <?php
+    include 'config/config.php';
     session_start();
-    require "templates/hello-world.php";
-    require "templates/nav.php";
+
+    $msg = "";
+
+    if(isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    }
+
+    
 ?>
 
 <html>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Camper Project</title>
-    <link href="link/stylishish.css" rel="stylesheet" type="text/css" />
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Instructor Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 
 <body>
-
-    <?php include("templates/nav.php"); ?>
-
-    <div class="main">
-        <form action="login.php" method="post">
-            <h2>Login</h2>
-            <p>Username: <input type="username" name="username" id="username" /></p>
-            <br />
-            <p>Password: <input type="password" name="password" id="password" /></p>
-            <input type="submit" name="login" value="Submit">
-        </form>
+    <?php include("templates/nav.php"); ?> 
+    <div class="container" style="margin-top:100px;">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-md-offset-3" align="center">
+                <form action="login.php" method="post">
+                    <h2>Instructor Login</h2>
+                    <!--<?php if($msg = "") echo $msg . "<br><br>" ?>-->
+                    <input class="form-control" minlength="3" name="username" placeholder="Username"><br>
+                    <input class="form-control" minlength="5" name="password" placeholder="Password" type="password"><br>
+                    <input class="btn btn-primary" type="submit" name="login" value="Submit">
+                </form>
+            </div>
+        </div>
     </div>
-
-
 </body>
-
 </html>
